82ac93a8a8171a65f0fce0648ae519d8f52cdd51
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-01-06 16:06:39 -0500

    Implement submit button functionality for adding book info

--- script.js
+++ script.js
@@ -5,40 +5,42 @@ let unorderList = document.querySelector('ul')
 
 
 addNewBook.addEventListener('click', ()=>{
-	console.log('Clicked')
+	let inputTitle = document.createElement('input')
+	let inputAuthor = document.createElement('input')
+	let inputPages = document.createElement('input')
+	let submit = document.createElement('button')
 	let inputWindow = document.createElement('div')
+	
+	console.log('Clicked')
+
 	inputWindow.height = 400;
 	inputWindow.width = 400;
 	inputWindow.setAttribute('id', 'inputWindow')
 	document.body.appendChild(inputWindow)
-	let inputTitle = document.createElement('input')
+
 	inputTitle.height = 200;
 	inputTitle.width = 200;
 	inputTitle.setAttribute('id', 'inputTitle')
 	inputWindow.appendChild(inputTitle)
-	let inputAuthor = document.createElement('input')
+
 	inputAuthor.height = 200;
 	inputAuthor.width = 200;
 	inputAuthor.setAttribute('id', 'inputAuthor')
 	inputWindow.appendChild(inputAuthor)
-	let inputPages = document.createElement('input')
+
 	inputPages.height = 200;
 	inputPages.width = 200;
 	inputPages.setAttribute('id', 'inputPages')
 	inputPages.setAttribute('type','number')
 	inputWindow.appendChild(inputPages)
-	let submit = document.createElement('button')
+
 	submit.setAttribute('id','submit')
 	submit.setAttribute('type','submit')
 	submit.textContent = 'SUBMIT'
 	inputWindow.appendChild(submit)
+
 	submit.addEventListener('click',() =>{
-		let addBook = new Book(inputTitle.value,inputAuthor.value,inputPages.value,false);
-		let newBook = document.createElement('li')
-		newBook.textContent = `${addBook.title} ${addBook.author} ${addBook.pages} ${addBook.read}`
-		unorderList.appendChild(newBook);
-		myLibrary.push(newBook)
-		console.log(myLibrary)
+		createBooks()
 		inputWindow.remove()
 	})
 
@@ -57,6 +59,33 @@ function Book(title,author,pages,read){
 	}
 }
 
+function createBooks(){
+	let addBook = new Book(inputTitle.value,inputAuthor.value,inputPages.value,false);
+	let newBook = document.createElement('li')
+	unorderList.appendChild(newBook)
+	let title = document.createElement('h1');
+	let author = document.createElement('h2');
+	let pages = document.createElement('h2')
+	let read = document.createElement('h2')
+	
+	newBook.appendChild(title);
+	newBook.appendChild(author);
+	newBook.appendChild(pages);
+	newBook.appendChild(read);
+	
+	title.textContent = addBook.title
+	author.textContent = addBook.author
+	pages.textContent = addBook.pages
+	read = addBook.read
+
+	myLibrary.push(newBook)
+	console.log(myLibrary)
+}
+
+
+function removeBooks(){
+	
+}
 
 
 addBook.info()
